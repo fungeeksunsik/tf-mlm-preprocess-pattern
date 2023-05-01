6c9d8d8e69c72efdededc08cb3a1155cbb156063
fungeeksunsik <siksicking@gmail.com>
2023-05-01 15:24:20 +0900

    imp: move preprocess logic from main module to preprocess module

--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,6 @@
+import logging
+import sys
+import config
 import requests
 import pathlib
 import shutil
@@ -97,3 +100,37 @@ def train_and_save_tokenizer(spm_trainer_config: Dict[str, Union[int, str, bool]
     :return: train and save sentencepiece tokenizer and return nothing
     """
     spm.SentencePieceTrainer.Train(**spm_trainer_config)
+
+
+if __name__ == "__main__":
+    local_dir = pathlib.Path(config.LOCAL_DIR)
+    local_dir.mkdir(exist_ok=True, parents=True)
+
+    formatter = logging.Formatter(
+        fmt="%(asctime)s (%(funcName)s) : %(msg)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
+    logger = logging.getLogger(__name__)
+    logger.addHandler(stream_handler)
+    logger.setLevel(logging.INFO)
+
+    logger.info("Download IMDb dataset and unpack archive")
+    imdb_path = download_and_unpack_imdb(
+        source_url=config.SOURCE_URL,
+        local_dir=local_dir,
+        archive_name=config.ARCHIVE_NAME,
+    )
+
+    logger.info("Extract review data from unpacked archive")
+    imdb_data = extract_data_from_imdb(imdb_path)
+
+    logger.info("Split dataset into train, test and save each respectively")
+    split_save_imdb_data(imdb_data, local_dir)
+
+    logger.info("Extract reviews from train data and save into local directory")
+    extract_corpus(local_dir)
+
+    logger.info("Prepare trained tokenizer using extracted corpus")
+    train_and_save_tokenizer(config.SPM_TRAINER_CONFIG)
